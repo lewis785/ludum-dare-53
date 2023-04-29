e3ceb2a28522e967ca56250aa0522ec6753c043f
Kerri Knier <kerriknier@gmail.com>
2023-04-29 13:49:02 +0100

    fix type in function call breaking the enemy scene

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mob_types : PackedStringArray = $AnimatedSprite2D.sprite_frames.get_animatimation_names()
+	var mob_types : PackedStringArray = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
 
