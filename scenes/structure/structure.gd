95e5c5296f2662f5a44ab806c970bb3746e7bfbf
David M Thomson <ld@dmt.dev>
2023-04-29 16:43:59 +0100

    feat: add attack logic

--- scenes/structure/structure.gd
+++ scenes/structure/structure.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var owned: bool = true
 @export var supplies: int = 100
 @export var tick_threshold: float = 1
-@export var enemy_name = '@enemy@'
+@export var parent_name = 'spawn'
 @export var label: Label
 @export var is_tower: bool
 @export var structure_damage: int = 10
@@ -15,7 +15,7 @@ var enemies: Array[RigidBody2D] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label = $TownHealth
+	label = $StructureHealth
 	label.set_text(str(health))
 	pass # Replace with function body.
 
@@ -28,10 +28,10 @@ func _process(delta):
 
 
 func can_take_damage():
-	return owned and tick
+	return owned
 
 func can_attack():
-	return is_tower and tick
+	return is_tower
 
 func subtract_damage_from_health(damage):
 	health -= damage
@@ -44,20 +44,22 @@ func reset_tick():
 	tick = false
 
 func state_manager():
-	if can_take_damage():
-		process_all_enemies_damage()
-		update_health_label(health)
-	if can_attack():
-		process_all_attacks()
 	if health <= 0:
 		update_health_label(0)
 		owned = false
+		return
+	if tick:
+		if can_take_damage():
+			process_all_enemies_damage()
+			update_health_label(health)
+		if can_attack():
+			process_all_attacks()
+		reset_tick()
+
 
 func process_all_enemies_damage():
 	for enemy in enemies:
 		subtract_damage_from_health(enemy.damage)
-	reset_tick()
-	pass
 
 func subtract_damage_from_enemies(enemy):
 	enemy.health -= structure_damage
@@ -66,8 +68,6 @@ func process_all_attacks():
 	for enemy in enemies:
 		subtract_damage_from_enemies(enemy)
 		break
-	reset_tick()
-	pass
 
 func tick_manager(delta):
 	time_til_tick += delta
@@ -76,14 +76,16 @@ func tick_manager(delta):
 	pass
 	
 
-func _on_town_area_2d_body_entered(body):
-	if str(body.name).begins_with(enemy_name):
+func _on_structure_area_2d_body_entered(body):
+	var body_parent_name = body.get_parent().name
+	if str(body_parent_name).begins_with(parent_name):
 		enemies.append(body)
 	pass # Replace with function body.
 
 
-func _on_town_area_2d_body_exited(body):
-	if str(body.name).begins_with(enemy_name):
+func _on_structure_area_2d_body_exited(body):
+	var body_parent_name = body.get_parent().name
+	if str(body_parent_name).begins_with(parent_name):
 		for i in range(enemies.size()):
 			if enemies[i].name == body.name:
 				enemies.erase(i)
