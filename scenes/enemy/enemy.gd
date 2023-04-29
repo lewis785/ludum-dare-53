3a538f4162fc2b58057a9d8c1a9a646def62d567
Kerri Knier <kerriknier@gmail.com>
2023-04-29 16:11:24 +0100

    trees assemble!

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -10,9 +10,28 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	var targets : Array[Node] = find_targets()
+	if len(targets) == 0 :
+		return
+	move_to_target(targets[0])
+	
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+func find_targets() -> Array[Node]:
+	var parent : Node = self.get_parent()
+	var targets : Array[Node] = parent.find_children("", "Marker2D")
+	print(len(targets), " targets")
+	
+	return targets
+	
+func move_to_target(target : Marker2D) -> void:
+	print(target.position)
+	
+	var vector : Vector2 = (target.position - self.position).normalized()
+	
+	self.linear_velocity = vector*linear_velocity.length()
+	
