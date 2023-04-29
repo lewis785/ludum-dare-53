c1a99472a7e40c503c3188ad422522c0e158e482
Nicholas Robinson <Nicholas23Robinson@gmail.com>
2023-04-30 00:38:14 +0100

    Fixed Town Spawn Scaling

--- scenes/map/map_populate.gd
+++ scenes/map/map_populate.gd
@@ -2,9 +2,9 @@ var width = 900;
 var height = 900;
 var map_sectors;
 
-var sector_width = width / 3
-var sector_height = height / 3
-var town_turret_range = sector_width/10
+var sector_width
+var sector_height
+var town_turret_range
 
 var tilemap
 var tree
@@ -24,6 +24,10 @@ func _init(local_tilemap, local_tree, local_width, local_height, local_map_secto
 	width = local_width
 	height = local_height
 	map_sectors = local_map_sectors
+	
+	sector_width = width / 3
+	sector_height = height / 3
+	town_turret_range = sector_width/10
 	
 	populate_map()
 
@@ -53,7 +57,7 @@ func fill_spawn_sector():
 
 func spawn_structure(x,y):
 	if structure:
-		var local_position =  tilemap.map_to_local(Vector2i(x,y)) /3
+		var local_position =  tilemap.map_to_local(Vector2i(x,y))
 		var instanced_structure = structure.instantiate()
 		tree.current_scene.add_child.call_deferred(instanced_structure)
 		instanced_structure.global_position = tilemap.to_global(local_position)
