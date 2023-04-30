13941d6226c45844cc25d674deb101b57527e4a2
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-04-30 02:32:33 +0100

    CHANGE only allow trucks when supply is available

--- scenes/supply_truck/truck_coordinator.gd
+++ scenes/supply_truck/truck_coordinator.gd
@@ -5,6 +5,7 @@ const truck = preload("res://scenes/supply_truck/supply_truck.tscn")
 
 @export var truck_scene : PackedScene
 var start_location: Vector2
+var depot_supplies
 
 func structures(nodes : Array[Node]) -> Array[Structure]:
 	var structures : Array[Structure] = []
@@ -19,15 +20,14 @@ func structures(nodes : Array[Node]) -> Array[Structure]:
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var start_location = self.position
-	#var truck: SupplyTruck = truck_scene.instantiate()
-	#var root = self.get_tree().current_scene
-	#var structs = structures(root.find_children('*structure*'))
-	
-	#truck.target_structure = structs[1]
-	#add_child(truck)
+	var depot_supplies = self.get_parent().supply_store_instance
 	pass
 
 func send_truck(location: Vector2):
+	if (depot_supplies && !depot_supplies.has_required_supplies(20)):
+		return
+	
+	depot_supplies.remove_supply(20)
 	var truck: SupplyTruck = truck_scene.instantiate()
 	truck.target_structure = location
 	add_child(truck)
@@ -39,5 +39,6 @@ func _input(event):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	pass
+	if (depot_supplies == null):
+		depot_supplies = self.get_parent().supply_store_instance
+		return
