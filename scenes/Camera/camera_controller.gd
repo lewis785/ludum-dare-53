5c0cc558e0be9c1b42cbaf713b99c133a4ba630c
Nicholas Robinson <Nicholas23Robinson@gmail.com>
2023-04-30 04:30:45 +0100

    Moved Script from Camera to parent node - Moving script allows Camera scene to have easily modifable parameters

--- scenes/Camera/camera_controller.gd
+++ scenes/Camera/camera_controller.gd
@@ -1,13 +1,15 @@
-extends Camera2D
+extends Node
 
-@export var camera_speed = 20
-@export var camera_zoom_speed = 0.5
+@export var camera_speed = 30
+@export var camera_zoom_speed = 0.9
 
+var camera
 var max_camera_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	max_camera_speed = camera_speed
+	camera = $Camera2D
 	pass # Replace with function body.
 
 
@@ -16,7 +18,7 @@ func _process(delta):
 	get_input(delta)
 
 func get_input(delta):
-	var movement = position
+	var movement = camera.position
 	if Input.is_action_pressed("camera_right"):
 		movement.x += camera_speed
 	if Input.is_action_pressed("camera_left"):
@@ -26,9 +28,9 @@ func get_input(delta):
 	if Input.is_action_pressed("camera_down"):
 		movement.y += camera_speed
 	if Input.is_action_pressed("camera_zoom_in"):
-		set_zoom(get_zoom() + ((camera_zoom_speed * get_zoom())/10))
+		camera.set_zoom(camera.get_zoom() + ((camera_zoom_speed * camera.get_zoom())/10))
 	if Input.is_action_pressed("camera_zoom_out"):
-		set_zoom(get_zoom() - ((camera_zoom_speed * get_zoom())/10))
+		camera.set_zoom(camera.get_zoom() - ((camera_zoom_speed * camera.get_zoom())/10))
 	
-	camera_speed = max_camera_speed / ((get_zoom().x+1) / 2)
-	position = movement
+	camera_speed = max_camera_speed / ((camera.get_zoom().x+1) / 2)
+	camera.position = movement
