4aaea573274f35c67821d1b834f083c2861bc93c
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-04-30 12:20:40 +0100

    ADD supply store scene to disply current supply

--- scenes/supply_depot/supply_depot.gd
+++ scenes/supply_depot/supply_depot.gd
@@ -1,24 +1,21 @@
 extends Node2D
 
-
 @export var max_supply = 100
 @export var refill_rate = 10
 @export var supply = 0
 var acc = 0
-const supply_store = preload("res://scripts/supply_store.gd")
-var supply_store_instance
 
 func _ready():
-	supply_store_instance = supply_store.new()
-	supply_store_instance.supply_capacity = max_supply
-	supply_store_instance.supplies = supply
+	
+	$truck_coordinator/SupplyStore.supply_capacity = max_supply
+	$truck_coordinator/SupplyStore.supplies = supply
 	$RefillTimer.start()
 	$DepotSprite.play('depot-healthy')
 
 func refill():
 	if max_supply == supply:
 		return
-	supply_store_instance.add_supply(refill_rate)
+	$truck_coordinator/SupplyStore.add_supply(refill_rate)
 	
 func _on_refill_timer_timeout():
 	refill()
