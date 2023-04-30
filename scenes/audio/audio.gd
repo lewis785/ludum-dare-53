350aeb4168f81ef04d6109cde2f3e76df3a3f06b
Nicholas Robinson <Nicholas23Robinson@gmail.com>
2023-04-30 22:55:33 +0100

    Added volume input to AudioManager

--- scenes/audio/audio.gd
+++ scenes/audio/audio.gd
@@ -5,9 +5,8 @@ var asp : AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#asp = get_tree().get_first_node_in_group("AudioPlayer")
+	asp = $AudioStreamPlayer
 	pass
-	#play_audio()
 	
 func get_asp():
 	asp = $AudioStreamPlayer
@@ -15,12 +14,11 @@ func get_asp():
 func play_audio():
 	asp.play()
 
-func adjust_pitch(pitch):
-	if !asp:
-		get_asp()
-	asp.set_pitch_scale(pitch)
-
 func stop_audio():
-	if !asp:
-		get_asp()
 	asp.stop()
+
+func set_volume(volume : float):
+	asp.set_volume_db(volume)
+
+func adjust_pitch(pitch):
+	asp.set_pitch_scale(pitch)
