842544d0daf38651647bde9d7335cc9514dc8713
Nicholas Robinson <Nicholas23Robinson@gmail.com>
2023-04-30 20:08:50 +0100

    Added Camera Edge Limits - Added camera link to bridge Camera and Map

--- scenes/Camera/camera_controller.gd
+++ scenes/Camera/camera_controller.gd
@@ -1,15 +1,21 @@
 extends Camera2D
+class_name Camera
 
 @export var camera_speed = 30
 @export var camera_zoom_speed = 0.9
+@export var limit_camera = true
 
-var max_camera_speed
+var max_camera_speed = camera_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	max_camera_speed = camera_speed
-	pass # Replace with function body.
 
+func set_limits(upper_x, upper_y, lower_x, lower_y):
+	self.set_limit(0,upper_x)
+	self.set_limit(1,upper_y)
+	self.set_limit(2,lower_x)
+	self.set_limit(3,lower_y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,6 +23,7 @@ func _process(delta):
 
 func get_input(delta):
 	var movement = position
+	var zoom_change = false
 	if Input.is_action_pressed("camera_right"):
 		movement.x += camera_speed
 	if Input.is_action_pressed("camera_left"):
@@ -26,9 +33,11 @@ func get_input(delta):
 	if Input.is_action_pressed("camera_down"):
 		movement.y += camera_speed
 	if Input.is_action_pressed("camera_zoom_in"):
+		zoom_change = true
 		set_zoom(get_zoom() + ((camera_zoom_speed * get_zoom())/10))
 	if Input.is_action_pressed("camera_zoom_out"):
+		zoom_change = true
 		set_zoom(get_zoom() - ((camera_zoom_speed * get_zoom())/10))
-	
-	camera_speed = max_camera_speed / ((get_zoom().x+1) / 2)
+	if zoom_change:
+		camera_speed = max_camera_speed / ((get_zoom().x+1) / 2)
 	position = movement
