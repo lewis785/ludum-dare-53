0f4d73d0916652ae65ecb7cd2c0751879950c367
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-04-30 02:32:05 +0100

    CHANGE supply depot to use supply store

--- scenes/supply_depot/supply_depot.gd
+++ scenes/supply_depot/supply_depot.gd
@@ -1,26 +1,25 @@
 extends Node2D
 
+
 @export var max_supply = 100
 @export var refill_rate = 10
 @export var supply = 0
 var acc = 0
+const supply_store = preload("res://scripts/supply_store.gd")
+var supply_store_instance
 
 func _ready():
+	supply_store_instance = supply_store.new()
+	supply_store_instance.supply_capacity = max_supply
+	supply_store_instance.supplies = supply
+	$RefillTimer.start()
 	$DepotSprite.play('depot-healthy')
 
 func refill():
 	if max_supply == supply:
 		return
-	if supply + refill_rate > max_supply:
-		supply = max_supply
-	supply += refill_rate
-	print(supply)
-	pass
+	supply_store_instance.add_supply(refill_rate)
+	print(supply_store_instance.supplies)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	acc += delta
-	if acc >= 1:
-		refill()
-		acc = 0
-	pass
+func _on_refill_timer_timeout():
+	refill()
