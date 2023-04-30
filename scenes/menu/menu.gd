445542b4055da2698041dd2a4cae2f1287405c56
Kerri Knier <kerriknier@gmail.com>
2023-04-30 14:31:47 +0100

    add skip

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -6,6 +6,7 @@ var skipping : bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	paused = false
+	skipping = false
 	get_tree().paused = false
 	
 
@@ -26,10 +27,23 @@ func _on_pause_button_button_down() -> void:
 	var text = "pause"
 	if paused:
 		text += "d"
+		$CanvasLayer/skip_button.hide()
+	else:
+		$CanvasLayer/skip_button.show()
+		
 	
 	$CanvasLayer/pause_button.text = text
 			
 
 
 func _on_skip_button_button_down() -> void:
-	pass # Replace with function body.
+	skipping = !skipping
+	
+	var text = "skip"
+	if skipping:
+		text = "slow"
+		Engine.time_scale = 10
+	else:
+		Engine.time_scale = 1
+		
+	$CanvasLayer/skip_button.text = text
