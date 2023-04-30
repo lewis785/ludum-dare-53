daedf7ca910f60cd4cef3df72808a08f2468fd86
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-04-30 22:52:41 +0100

    FIX trucwk

--- scenes/supply_truck/truck_coordinator.gd
+++ scenes/supply_truck/truck_coordinator.gd
@@ -58,7 +58,7 @@ func send_truck(structure: Structure):
 	var truck: SupplyTruck = truck_scene.instantiate()
 	truck.target_structure = structure
 	truck.truck_indicator = truck_indicator
-	truck.trucwk_id = truck_indicator.next_free_truck()
+	truck.truck_id = truck_indicator.next_free_truck()
 	
 	add_child(truck)
 
