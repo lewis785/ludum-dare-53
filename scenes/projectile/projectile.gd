8bc29ca4db5848db548ebaa2ba6ecc9f355ddf4c
Kerri Knier <kerriknier@gmail.com>
2023-05-01 17:09:02 +0100

    reduce chance of hitting nil in projectile

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -2,7 +2,6 @@ class_name Projectile extends Area2D
 
 @export var projectile_speed: int = 1000
 @export var projectile_damage: int = 10
-var _animated_sprite
 var _asp : AudioStreamPlayer2D
 
 var signal_bus
@@ -10,8 +9,6 @@ var signal_bus
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	signal_bus = get_node("/root/SignalBus")
-	_animated_sprite = $AnimatedSprite2D
-	#var path = self.get_path() + NodePath("/AudioStreamPlayer2D")
 	for child in get_children():
 		if child is AudioStreamPlayer2D:
 			_asp = child
@@ -20,7 +17,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	_animated_sprite.play()
+	$AnimatedSprite2D.play()
 	var direction = Vector2.RIGHT.rotated(rotation)
 	global_position += projectile_speed * direction * delta
 	pass
@@ -38,7 +35,7 @@ func _on_body_entered(body):
 	var enemy = body.find_child("entity")
 	
 	enemy.take_damage(projectile_damage)
-	if !enemy.active:
+	if !enemy.active and signal_bus:
 		signal_bus.emit_signal("enemy_killed", body)
 		
 	$AnimatedSprite2D.hide()
